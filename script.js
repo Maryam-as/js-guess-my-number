3cb357e7cc30f8aebb5d874ddab3d479045e46fc
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-04-20 18:37:59 +0330

    reveal secret number and update styling on win

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -19,6 +26,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
